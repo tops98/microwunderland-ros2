8ceb1345a0c44535f5bd911ef514be1dff712d9d
Tobias Haugg <tobias.haugg@gmail.com>
2023-06-05 12:18:16 +0200

    add max time and max sample option

--- src/src_/image_exporter/image_exporter/image_exporter_node.py
+++ src/src_/image_exporter/image_exporter/image_exporter_node.py
@@ -5,10 +5,13 @@ from pathlib import Path
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from datetime import datetime
+import sys
 
 
 class ImageExporterNode(Node):
 
+    _count: int
+    _num_exported: int
     _export_path: str
     _image_stream_topic: str
     _buffer: CompressedImage
@@ -22,6 +25,7 @@ class ImageExporterNode(Node):
         self.get_logger().info("starting node")
 
         self._count = 0
+        self._num_exported = 0
         self._bridge = CvBridge()
         self._buffer = None
         self._last_buff_val = None
@@ -30,15 +34,27 @@ class ImageExporterNode(Node):
         self.declare_parameter("export_path","image_exports")
         self.declare_parameter("nth_sample",1)
         self.declare_parameter("time_intervall",0.0)
+        self.declare_parameter("max_time",0.0)
+        self.declare_parameter("max_exports",0)
+
+
+        self._max_time = self.get_parameter("max_time").get_parameter_value().double_value
+        self._max_exports = self.get_parameter("max_exports").get_parameter_value().integer_value
 
         self._time_intervall = self.get_parameter("time_intervall").get_parameter_value().double_value
         self._nth_frame = self.get_parameter("nth_sample").get_parameter_value().integer_value
+
         self._export_path = self.get_parameter("export_path").get_parameter_value().string_value
         self._image_stream_topic = self.get_parameter("topic_name").get_parameter_value().string_value
 
-        self.get_logger().info(f"setting intervall to {self._time_intervall} seconds")
+        if self._max_time > 0:
+            self.get_logger().info(f"setting max execution time to {self._max_time} seconds")
+            self.create_timer(self._max_time,self.shut_down)
+
         if self._time_intervall > 0:
+            self.get_logger().info(f"setting intervall to {self._time_intervall} seconds")
             self.create_timer(self._time_intervall,self.timer_cb)
+
 
         self.create_subscription(CompressedImage,self._image_stream_topic,self.img_received_cb,10)
         self.get_logger().info(f"subscribed to topic: {self._image_stream_topic}")
@@ -64,8 +80,17 @@ class ImageExporterNode(Node):
             export_path = f"{self._export_path}/{datetime.now()}.jpg"    
             cv2Img = self._bridge.compressed_imgmsg_to_cv2(cmprs_img_msg=msg)
             cv2.imwrite(export_path, cv2Img)
+            self._num_exported += 1
             self.get_logger().info(f"wrote image to: {export_path}")
-    
+
+        if self._num_exported == self._max_exports:
+            self.shut_down()
+
+
+    def shut_down(self) -> None:
+        self.destroy_node()
+        sys.exit()
+        
 
     def check_if_export_allowed(self, img_msg:CompressedImage) -> bool:
         self._count = (self._count +1) % self._nth_frame
