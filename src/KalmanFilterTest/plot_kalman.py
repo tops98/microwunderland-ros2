9c05632f4388731cc306c8bfc169ae673155c661
Tobias Haugg <tobias.haugg@gmail.com>
2023-09-07 12:22:31 +0200

    tuned hyperparameter (higher estimation uncertainty lower process noise)

--- src/KalmanFilterTest/plot_kalman.py
+++ src/KalmanFilterTest/plot_kalman.py
@@ -18,15 +18,15 @@ measurment_uncertainty = np.array([
 ])
 
 processnoise = np.array([
-    0.1,   # position noise
-    0.1,   # velocity noise
-    0.1   # accelaration noise
+    0.045,   # position noise
+    0.045,   # velocity noise
+    0.045   # accelaration noise
 ])
 
 estimatation_uncertainty = np.array([
-    0.001,  # error in initial position estimate
-    .025,   # error in initial velocity estimate
-    .05   # error in initial accelaration estimate
+    1,  # error in initial position estimate
+    5,   # error in initial velocity estimate
+    10   # error in initial accelaration estimate
 ])
 
 time_step = 0.1
@@ -39,27 +39,29 @@ pos_estimates = np.zeros(iterations)
 vel_estimates = np.zeros(iterations)
 acc_estimates = np.zeros(iterations)
 variances = np.zeros((3,iterations))
+error = 0
 
 
-filter = CVD_KalmanFilter(dim_x=3, dim_z=1,initial_state=initial_state)
-filter._P = estimatation_uncertainty.dot(estimatation_uncertainty.transpose())
-filter._R = measurment_uncertainty.dot(measurment_uncertainty.transpose())
-filter._Q = processnoise.dot(processnoise.transpose())
 
+for j in range(2000):
+    filter = CVD_KalmanFilter(dim_x=3, dim_z=1,initial_state=initial_state)
+    filter._P = estimatation_uncertainty.dot(estimatation_uncertainty.transpose())
+    filter._R = measurment_uncertainty.dot(measurment_uncertainty.transpose())
+    filter._Q = processnoise.dot(processnoise.transpose())
+    for i in range(iterations):
+        measurment = ground_truth[i]+ np.random.uniform(-1, 1)*measurment_uncertainty
+        filter.predict(time_step)
+        if not (i >100 and i < 200):
+            if i % update_rate == 0:
+                filter.update(measurments=measurment)
 
-for i in range(iterations):
-    measurment = ground_truth[i]+ np.random.uniform(-1, 1)*measurment_uncertainty
-    filter.predict(time_step)
-    if not (i >100 and i < 200):
-        if i % update_rate == 0:
-            filter.update(measurments=measurment)
+        measurments[i] = measurment
+        pos_estimates[i] = filter._H.dot(filter._X)
+        variances[:,i] = filter._P.diagonal()
 
-    measurments[i] = measurment
-    pos_estimates[i] = filter._H.dot(filter._X)
-    variances[:,i] = filter._P.diagonal()
-
-    vel_estimates[i] = filter._X[1]
-    acc_estimates[i] = filter._X[2]
+        vel_estimates[i] = filter._X[1]
+        acc_estimates[i] = filter._X[2]
+        error += abs(filter._X[0] - ground_truth[i])
 
 pyplot.subplot(3,1,1)
 pyplot.plot(ground_truth,'-k')
@@ -91,4 +93,5 @@ pyplot.ylabel("m/s")
 pyplot.legend(["true accelaration", "estimate", "variance"],loc='center left', bbox_to_anchor=(1, 0.5))
 
 pyplot.tight_layout(h_pad=0.5)
+print(error/(iterations*2000))
 pyplot.show()
