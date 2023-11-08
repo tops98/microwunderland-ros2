4c032b4db619cd3f320e556d9f8ddb5630cead41
Tobis_laptop\Tobi <tobias.haugg@haw-hamburg.de>
2023-11-08 22:05:33 +0100

    add new traffic planner

--- src/src_/traffic_planner/submodules/data_classes.py
+++ src/src_/traffic_planner/submodules/data_classes.py
@@ -1,28 +1,54 @@
 from dataclasses import dataclass
-from typing import List
-import numpy.typing as npt
+from typing import List, Dict
+from enum import Enum
+import numpy as np
+from rclpy.node import Client
+
+
+class BlinkerStates (Enum):
+    OFF = 0
+    LEFT = 1
+    RIGHT = 2
+    HAZARD = 3
+class TaillightStates (Enum):
+    ON = 0
+    OFF = 1
+    BRAKE = 2
+class HeadlightStates (Enum):
+    OFF = 0
+    ON = 1
+
+@dataclass
+class TinycarService:
+    battery_service:Client
+    speed_service:Client
+    headlight_service:Client
+    taillight_service:Client
+    blinker_service:Client
+
 
 @dataclass
 class MapNode:
     name:str
-    position:npt.ArrayLike
+    position:np.ndarray
     trigger_radius:float
-    connections:List[str]
+    connections:list
 
-@dataclass
+@dataclass 
 class Checkpoint:
-    node:MapNode
-    state:str
+    map_node: MapNode
+    actions:Dict[str, any]
 
 @dataclass
 class TravelPlan:
-    start_time: float
-    loop: bool
-    next_checkpoint: int
-    checkpoints:List[Checkpoint]
-    
-@dataclass
-class TrackedObject:
     name:str
-    position:npt.ArrayLike
-    travel_plan: TravelPlan
+    loop_plan:bool
+    checkpoints:List[Checkpoint]
+
+@dataclass
+class ActiveTravelPlan:
+    tracker_id:int
+    tiny_car_address:str
+    next_checkpoint:int
+    travel_plan:TravelPlan
+    pause:bool
